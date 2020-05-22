4243e54f6c13b0462222fcf2ae02dce0dd3b331c
Nava Krishnan <nava.arul@gmail.com>
2020-05-22 05:04:17 +0100

    Use regex to replace non alphanumeric characters in category name

--- app/controller/category.js
+++ app/controller/category.js
@@ -15,7 +15,7 @@ exports.create = (req, res) => {
         });
     }
     if (req.body.parent) {
-        Category.exists({"_id":req.body.parent}, function(err, result){
+        Category.exists({ "_id": req.body.parent }, function (err, result) {
             if (err) {
                 return res.status(500).send({
                     message: "Error while finding Category with id " + req.body.parent
@@ -23,13 +23,13 @@ exports.create = (req, res) => {
             } else if (result) {
                 checkDuplicateAndPersist(req, res);
             } else {
-                console.log("Cannot create category. The parent category " + req.body.parent+" not found");
+                console.log("Cannot create category. The parent category " + req.body.parent + " not found");
                 return res.status(400).send({
-                    message: "Cannot create category. The parent category " + req.body.parent+" not found"
+                    message: "Cannot create category. The parent category " + req.body.parent + " not found"
                 });
             }
         });
-    }else{
+    } else {
         checkDuplicateAndPersist(req, res);
     }
 };
@@ -37,7 +37,7 @@ exports.create = (req, res) => {
 
 function checkDuplicateAndPersist(req, res) {
     console.log("Checking if a Category already exist with name " + req.body.name);
-    Category.exists({ name: req.body.name }, function (err, result) {
+    Category.exists({ name: req.body.name, parent: req.body.parent }, function (err, result) {
         if (err) {
             return res.status(500).send({
                 message: "Error while finding Category with name " + req.body.name
@@ -53,23 +53,7 @@ function checkDuplicateAndPersist(req, res) {
     });
 }
 
-function persist(req, res) {
-    const category = new Category({
-        name: req.body.name,
-        slug: req.body.slug || req.body.name.split(" ").join("-").trim().toLowerCase(),
-        parent: req.body.parent
-    });
-    // Save Note in the database
-    category.save()
-        .then(data => {
-            console.log("Persisted Category: " + data._id);
-            res.status(201).send(data);
-        }).catch(err => {
-            res.status(500).send({
-                message: err.message || "Some error occurred while creating the Category."
-            });
-        });
-}
+
 
 // Retrieve and return all categories from the database.
 exports.findAll = (req, res) => {
@@ -112,60 +96,94 @@ exports.findOne = (req, res) => {
 
 // Update a Category identified by the BrandId in the request
 exports.update = (req, res) => {
-    console.log("Updating Category "+ JSON.stringify(req.body));
+    console.log("Updating Category " + JSON.stringify(req.body));
     // Validate Request
-   if(!req.body) {
-       return res.status(400).send({
-           message: "Category body can not be empty"
-       });
-   }
-   if(!req.body.name) {
-       return res.status(400).send({
-           message: "Category name can not be empty"
-       });
-   }
-   // Find Category and update it with the request body
-   Category.findByIdAndUpdate(req.params.id, {
-       name: req.body.name,
-       slug: req.body.slug || req.body.name.split(" ").join("-").trim().toLowerCase()
-   }, {new: true})
-   .then(Category => {
-       if(!Category) {
-           return res.status(404).send({
-               message: "Category not found with id " + req.params.id
-           });
-       }
-       res.send(Category);
-   }).catch(err => {
-       if(err.kind === 'ObjectId') {
-           return res.status(404).send({
-               message: "Category not found with id " + req.params.id
-           });                
-       }
-       return res.status(500).send({
-           message: "Error updating Category with id " + req.params.id
-       });
-   });
+    if (!req.body) {
+        return res.status(400).send({
+            message: "Category body can not be empty"
+        });
+    }
+    if (!req.body.name) {
+        return res.status(400).send({
+            message: "Category name can not be empty"
+        });
+    }
+    // Find Category and update it with the request body
+    Category.findByIdAndUpdate(req.params.id, {
+        name: req.body.name,
+        slug: req.body.slug || req.body.name.trim().replace(/[\W_]+/g, "-").toLowerCase(),
+        parent: req.body.parent
+    }, { new: true })
+        .then(Category => {
+            if (!Category) {
+                return res.status(404).send({
+                    message: "Category not found with id " + req.params.id
+                });
+            }
+            res.send(Category);
+        }).catch(err => {
+            if (err.kind === 'ObjectId') {
+                return res.status(404).send({
+                    message: "Category not found with id " + req.params.id
+                });
+            }
+            return res.status(500).send({
+                message: "Error updating Category with id " + req.params.id
+            });
+        });
 };
 
 // Delete a Category with the specified BrandId in the request
 exports.delete = (req, res) => {
     Category.findByIdAndRemove(req.params.id)
-    .then(Category => {
-        if(!Category) {
-            return res.status(404).send({
-                message: "Category not found with id " + req.params.id
+        .then(Category => {
+            if (!Category) {
+                return res.status(404).send({
+                    message: "Category not found with id " + req.params.id
+                });
+            }
+            res.send({ message: "Category deleted successfully!" });
+        }).catch(err => {
+            if (err.kind === 'ObjectId' || err.name === 'NotFound') {
+                return res.status(404).send({
+                    message: "Category not found with id " + req.params.id
+                });
+            }
+            return res.status(500).send({
+                message: "Could not delete Category with id " + req.params.id
             });
-        }
-        res.send({message: "Category deleted successfully!"});
-    }).catch(err => {
-        if(err.kind === 'ObjectId' || err.name === 'NotFound') {
-            return res.status(404).send({
-                message: "Category not found with id " + req.params.id
-            });                
-        }
-        return res.status(500).send({
-            message: "Could not delete Category with id " + req.params.id
         });
-    });
 };
+
+/**
+ * Persists new Category Model 
+ * 
+ * @param {*} req The HTTP Request 
+ * @param {*} res The HTTP Response
+ */
+function persist(req, res) {
+    const category = buildCategory(req);
+    // Save Note in the database
+    category.save()
+        .then(data => {
+            console.log("Persisted Category: " + data._id);
+            res.status(201).send(data);
+        }).catch(err => {
+            res.status(500).send({
+                message: err.message || "Some error occurred while creating the Category."
+            });
+        });
+}
+
+/**
+ * Builds Category from incoming Request.
+ * @returns Category Model
+ * @param {*} req 
+ */
+function buildCategory(req) {
+    return new Category({
+        name: req.body.name,
+        slug: req.body.slug || req.body.name.trim().replace(/[\W_]+/g, "-").toLowerCase(),
+        parent: req.body.parent
+    });
+}
--- app/controller/product.js
+++ app/controller/product.js
@@ -1,26 +1,172 @@
-const Product = require('../model/product.js');
+const Product = require('../model/product');
 
 // Create and Save a new Product
 exports.create = (req, res) => {
-
+    console.log("Creating new Product " + JSON.stringify(req.body));
+    // Validate Request
+    if (!req.body) {
+        return res.status(400).send({
+            message: "Product body can not be empty"
+        });
+    }
+    if (!req.body.name) {
+        return res.status(400).send({
+            message: "Product name can not be empty"
+        });
+    }
+    if (req.body.parent) {
+        Product.exists({"_id":req.body.parent}, function(err, result){
+            if (err) {
+                return res.status(500).send({
+                    message: "Error while finding Product with id " + req.body.parent
+                });
+            } else if (result) {
+                checkDuplicateAndPersist(req, res);
+            } else {
+                console.log("Cannot create Product. The parent Product " + req.body.parent+" not found");
+                return res.status(400).send({
+                    message: "Cannot create Product. The parent Product " + req.body.parent+" not found"
+                });
+            }
+        });
+    }else{
+        checkDuplicateAndPersist(req, res);
+    }
 };
 
-// Retrieve and return all Products from the database.
+
+function checkDuplicateAndPersist(req, res) {
+    console.log("Checking if a Product already exist with name " + req.body.name);
+    Product.exists({ name: req.body.name, parent: req.body.parent }, function (err, result) {
+        if (err) {
+            return res.status(500).send({
+                message: "Error while finding Product with name " + req.body.name
+            });
+        }
+        else if (result) {
+            console.log("Product already exist with name " + req.body.name);
+            res.status(400).send({ message: "Product already exist with name " + req.body.name });
+        }
+        else {
+            persist(req, res);
+        }
+    });
+}
+
+function persist(req, res) {
+    const Product = new Product({
+        name: req.body.name,
+        slug: req.body.slug || req.body.name.split(" ").join("-").trim().toLowerCase(),
+        parent: req.body.parent
+    });
+    // Save Note in the database
+    Product.save()
+        .then(data => {
+            console.log("Persisted Product: " + data._id);
+            res.status(201).send(data);
+        }).catch(err => {
+            res.status(500).send({
+                message: err.message || "Some error occurred while creating the Product."
+            });
+        });
+}
+
+// Retrieve and return all categories from the database.
 exports.findAll = (req, res) => {
-
+    console.log("Received request to get all categories");
+    Product.find()
+        .then(data => {
+            if (data) {
+                console.log("Returning " + data.length + " categories.");
+                res.send(data);
+            } else {
+                console.log("Returning no categories ");
+                res.send({});
+            }
+        })
+        .catch(err => {
+            res.status(500).send({
+                message: err.message || "Some error occurred while retrieving categories."
+            });
+        });
 };
 
-// Find a single Product with a ProductId
+// Find a single Product with a BrandId
 exports.findOne = (req, res) => {
-
+    console.log("Received request get a Product with id " + req.params.id);
+    Product.findById(req.params.id)
+        .then(Product => {
+            if (!Product) {
+                return res.status(404).send({ message: "Product not found with id " + req.params.id });
+            }
+            res.send(Product);
+        }
+        )
+        .catch(err => {
+            if (err.kind === 'ObjectId') {
+                return res.status(404).send({ message: "Product not found with id " + req.params.id });
+            }
+            return res.status(500).send({ message: "Error while retrieving Product with id " + req.params.id });
+        });
 };
 
-// Update a Product identified by the ProductId in the request
+// Update a Product identified by the BrandId in the request
 exports.update = (req, res) => {
-
+    console.log("Updating Product "+ JSON.stringify(req.body));
+    // Validate Request
+   if(!req.body) {
+       return res.status(400).send({
+           message: "Product body can not be empty"
+       });
+   }
+   if(!req.body.name) {
+       return res.status(400).send({
+           message: "Product name can not be empty"
+       });
+   }
+   // Find Product and update it with the request body
+   Product.findByIdAndUpdate(req.params.id, {
+       name: req.body.name,
+       slug: req.body.slug || req.body.name.split(" ").join("-").trim().toLowerCase(),
+       parent: req.body.parent
+   }, {new: true})
+   .then(Product => {
+       if(!Product) {
+           return res.status(404).send({
+               message: "Product not found with id " + req.params.id
+           });
+       }
+       res.send(Product);
+   }).catch(err => {
+       if(err.kind === 'ObjectId') {
+           return res.status(404).send({
+               message: "Product not found with id " + req.params.id
+           });                
+       }
+       return res.status(500).send({
+           message: "Error updating Product with id " + req.params.id
+       });
+   });
 };
 
-// Delete a Product with the specified ProductId in the request
+// Delete a Product with the specified BrandId in the request
 exports.delete = (req, res) => {
-
+    Product.findByIdAndRemove(req.params.id)
+    .then(Product => {
+        if(!Product) {
+            return res.status(404).send({
+                message: "Product not found with id " + req.params.id
+            });
+        }
+        res.send({message: "Product deleted successfully!"});
+    }).catch(err => {
+        if(err.kind === 'ObjectId' || err.name === 'NotFound') {
+            return res.status(404).send({
+                message: "Product not found with id " + req.params.id
+            });                
+        }
+        return res.status(500).send({
+            message: "Could not delete Product with id " + req.params.id
+        });
+    });
 };
